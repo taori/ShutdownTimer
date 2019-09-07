220148d51b88917b379656c5072b63361bf70ec3
Andreas Müller <taori@users.noreply.github.com>
2019-09-07 18:53:01 +0200

    working state

---
+++ src/ShutdownTimer.Server/Authorization/OperationType.cs
@@ -0,0 +1,12 @@
+﻿namespace ShutdownTimer.Server.WindowsService.Authorization
+{
+	public enum OperationType
+	{
+		Shutdown,
+		AbortShutdown,
+		IsShutdownPending,
+		Hibernate,
+		Restart,
+		Logout
+	}
+}
